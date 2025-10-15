3af74c6069c299acc98715bf7d057a27e5744c5b
sangeetdhakal39-maker <sangeetdhakal39@gmail.com>
2025-10-15 23:27:11 +0545

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("profileBtn").addEventListener("click", function () {
+  const list = document.getElementById("accountList");
+  list.style.display = list.style.display === "block" ? "none" : "block";
+});
